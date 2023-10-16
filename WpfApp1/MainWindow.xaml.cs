fce75f0ce29f719f00b41b6116405efeaad6b9d9
User7 <User7@DESKTOP-JQK5V7P>
2023-10-16 11:04:21 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private bool IsMaximized = false;
+        private void Border_MouseLeftBattonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount ==  2) 
+            {
+                this.WindowState = WindowState.Normal;
+                this.Width = 1080;
+                this.Width = 720;
+
+                IsMaximized = false;
+            }
+            else
+            {
+                this.IsMaximized = WindowState.Maximized;
+            }
+        }
+
+    }
+}
